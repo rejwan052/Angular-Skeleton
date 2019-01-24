1fa2371a5f63dd8b371c007170eb48dbb7a596a3
Rejwan Ahmed <rejwan.052@gmail.com>
2019-01-25 00:37:17 +0600

    ## Location Strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 const routes: Routes = [
 	{
@@ -17,6 +18,7 @@ const routes: Routes = [
 	imports: [
 		RouterModule.forRoot(routes)
 	],
-	exports: [RouterModule]
+	exports: [RouterModule],
+	providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
